a2d58bf7bd99064d496601d6050fbb46c1efb1da
Raidel <rraidel85@gmail.com>
2024-03-18 06:29:37 -0700

    change users app to usuarios and add login form

---
+++ usuarios/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
+
+from usuarios.views import UserLoginView, ProfileDetailView, PasswordChangeDoneView
+
+app_name = 'usuarios'
+urlpatterns = [
+    path('login/', UserLoginView.as_view(template_name='usuarios/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('perfil/', ProfileDetailView.as_view(), name='perfil'),
+    path('cambiar-clave/', auth_views.PasswordChangeView.as_view(template_name='usuarios/profile/change_password.html',
+                                                                 success_url=reverse_lazy(
+                                                                     'usuarios:password_change_done')),
+         name='cambiar_clave'),
+    path('password-change_done/', PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+]
---
+++ usuarios/views.py
@@ -0,0 +1,33 @@
+from django.contrib import messages
+from django.contrib.auth import views
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views import View
+
+
+class UserLoginView(LoginView):
+    def form_valid(self, form):
+        messages.success(self.request, 'Bienvenid(o) al sistema')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('news:index')
+
+class PasswordChangeDoneView(LoginRequiredMixin, views.PasswordChangeDoneView):
+
+    def get(self, request, *args, **kwargs):
+        messages.success(self.request, 'Su contraseña se ha cambiado correctamente')
+        return HttpResponseRedirect(reverse('usuarios:perfil'))
+
+
+class ProfileDetailView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        return render(request, template_name='usuarios/profile/profile_detalles.html')
+
+
+
+
