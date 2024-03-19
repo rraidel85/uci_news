1f2d3001a429059300921dde2b6c5dfaae489641
Raidel <rraidel85@gmail.com>
2024-03-18 22:47:25 -0700

    change default user model

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email', 'phone_number', 'street', 'number', 'zone', 'category', 'is_staff']
+    list_filter = ['username', 'email', 'phone_number', 'street', 'number', 'zone', 'category', 'is_staff']
+    
+    fieldsets = (
+        *UserAdmin.fieldsets,  # original form fieldsets, expanded
+        (                      # new fieldset added on to the bottom
+            'Información adicional',  # group heading of your choice; set to None for a blank space instead of a header
+            {
+                'fields': (
+                    'phone_number',
+                    'street',
+                    'number',
+                    'zone',
+                    'category'
+                ),
+            },
+        ),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser    
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=20, verbose_name='Teléfono')    
+    street = models.CharField(max_length=255, verbose_name='Calle')
+    number = models.CharField(max_length=10, verbose_name='Número')
+    zone = models.CharField(max_length=255, verbose_name='Reparto')
+    category = models.CharField(max_length=255, verbose_name='Categoría Científica', blank=True, null=True)
+    
+    
