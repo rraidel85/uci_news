4dea1fd3a9cbc8d4fce2729a126ff41ba7972437
Raidel <rraidel85@gmail.com>
2024-03-19 11:23:46 -0700

    add login url to settings

--- uci_news/settings.py
+++ uci_news/settings.py
@@ -133,3 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'usuarios:login'
+LOGIN_REDIRECT_URL = 'news:list'
+LOGOUT_REDIRECT_URL = 'usuarios:login'
