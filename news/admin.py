19771dc8dc405f33c7b1bfe61c65c2cc73c8747f
Raidel <rraidel85@gmail.com>
2024-03-19 09:30:26 -0700

    add news and category to admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, News
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'pub_date', 'source', 'gender', 'ideological_stance', 'descriptor')
+    list_filter = ('category', 'pub_date')
+    search_fields = ('title', 'source', 'gender', 'ideological_stance', 'descriptor')
+    date_hierarchy = 'pub_date'
