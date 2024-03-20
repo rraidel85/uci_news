ae4f319aa549b8b0064c1cbfa68abfb41695b229
Raidel <rraidel85@gmail.com>
2024-03-19 18:37:47 -0700

    add category urls to app

--- news/urls.py
+++ news/urls.py
@@ -7,9 +7,16 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.NewsListView.as_view(), name='list'),
-    path('create/', views.NewsCreateView.as_view(), name='create'),
-    path('edit/<int:pk>/', views.NewsUpdateView.as_view(), name='edit'),
-    path('news/<int:pk>/', views.NewsDetailView.as_view(), name='detail'),
-    path('delete/<int:pk>/', views.NewsDeleteView.as_view(), name='delete'),
+    path('noticias/crear/', views.NewsCreateView.as_view(), name='create'),
+    path('noticias/editar/<int:pk>/', views.NewsUpdateView.as_view(), name='edit'),
+    path('noticias/<int:pk>/', views.NewsDetailView.as_view(), name='detail'),
+    path('noticias/eliminar/<int:pk>/', views.NewsDeleteView.as_view(), name='delete'),
+    
+    #Category
+    path('tematicas', views.NewsListView.as_view(), name='category_list'),
+    path('tematicas/crear/', views.NewsCreateView.as_view(), name='category_create'),
+    path('tematicas/editar/<int:pk>/', views.NewsUpdateView.as_view(), name='category_edit'),
+    path('tematicas/<int:pk>/', views.NewsDetailView.as_view(), name='category_detail'),
+    path('tematicas/eliminar/<int:pk>/', views.NewsDeleteView.as_view(), name='category_delete'),
 ]
 
